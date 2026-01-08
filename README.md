4e8940f2c5faaf844cf405b9c2d422960aab6041
Bruno Cruz <bmcruzz@hotmail.com>
2026-01-08 20:43:18 -0300

    feat: serverless devops lab with terraform remote state and ci/cd

---
+++ README.md
@@ -0,0 +1,2 @@
+# serverless-devops-lab
+Infraestrutura automatizada com pipeline GitHub Actions, Terraform e aplicação serverless Lambda, com monitoramento via CloudWatch
---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-devops-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "app" {
+  function_name = "devops-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+
+  filename      = "../lambda/lambda.zip"
+  source_code_hash = filebase64sha256("../lambda/lambda.zip")
+}
+
+resource "aws_apigatewayv2_api" "api" {
+  name          = "devops-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "integration" {
+  api_id             = aws_apigatewayv2_api.api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.app.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "route" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}
+
+resource "aws_lambda_permission" "api" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.app.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_stage" "stage" {
+  api_id = aws_apigatewayv2_api.api.id
+  name   = "$default"
+  auto_deploy = true
+}
